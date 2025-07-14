2c1f54c178f64f273fd92fffa5af325f73881e8e
Jorb1 <baileyjorgensen101@gmail.com>
2025-07-13 22:36:18 -0700

    add new file paths

--- shinydashboard/global.R
+++ shinydashboard/global.R
@@ -2,13 +2,13 @@
 librarian::shelf(shiny, shinydashboard, shinyWidgets, tidyverse, leaflet, shinycssloaders, fresh, sf, janitor)
 
 # Read in study regions boundaries
-sr_boundary.dir <- "/capstone/marinebiomaps/data/MLPA_Study_Regions"
+sr_boundary.dir <- "/MEDS/MBM/mbm_dashboard/data/MLPA_Study_Regions"
 sr_boundaries <- sf::st_read(file.path(sr_boundary.dir, "Marine_Life_Protection_Act_Study_Regions_-_R7_-_CDFW_[ds3178].shp")) |> 
   clean_names() |> 
   sf::st_transform('+proj=latlong +datum=WGS84')
 
 # Read in MPA boundaries
-boundary.dir <- "/capstone/marinebiomaps/data/MPA_boundaries"
+boundary.dir <- "/MEDS/MBM/mbm_dashboard/data/MPA_boundaries"
 mpas <- sf::st_read(file.path(boundary.dir, "California_Marine_Protected_Areas_[ds582].shp")) |> 
   clean_names() |>
   sf::st_transform('+proj=latlong +datum=WGS84') |> 
@@ -18,39 +18,44 @@ mpas <- sf::st_read(file.path(boundary.dir, "California_Marine_Protected_Areas_[
 
 # Data for NCSR study region
 
-# Read in ncsr substrate data
-rds.dir <- "/capstone/marinebiomaps/data/rds-files/"
-ncsr_substrate <- readRDS(file.path(rds.dir, "ncsr_substrate.rds")) |> 
-  sf::st_transform('+proj=latlong +datum=WGS84')
+rds.dir <- "/MEDS/MBM/mbm_dashboard/data//rds_files/"
 
-# ncsr mpa data
-ncsr_mpas <- mpas |> 
-  filter(study_regi == "NCSR")
+ncsr_substrate <- readRDS(file.path(rds.dir, "ncsr_substrate.rds")) |>
+  sf::st_transform('+proj=latlong +datum=WGS84')
 
 # NCSR biota data
-ncsr_biota <- readRDS(file.path(rds.dir, "ncsr_biota.rds")) |> 
+ncsr_biota <- readRDS(file.path(rds.dir, "ncsr_biota.rds")) |>
   sf::st_transform('+proj=latlong +datum=WGS84')
-
-
-
 
 
 
 
 # Data for NCCSR study region
-
-# Read in nccsr substrate data
-rds.dir <- "/capstone/marinebiomaps/data/rds-files/"
-nccsr_substrate <- readRDS(file.path(rds.dir, "nccsr_substrate.rds")) |> 
+nccsr_substrate <- readRDS(file.path(rds.dir, "nccsr_substrate.rds")) |>
   sf::st_transform('+proj=latlong +datum=WGS84')
 
-# nccsr mpa data
-nccsr_mpas <- mpas |> 
-  filter(study_regi == "NCCSR")
+nccsr_biota <- readRDS(file.path(rds.dir, "nccsr_biota.rds")) |> 
+  sf::st_transform('+proj=latlong +datum=WGS84')
+
+
+
+# CCSR 
+ccsr_substrate <- readRDS(file.path(rds.dir, "ccsr_substrate.rds")) |>
+  sf::st_transform('+proj=latlong +datum=WGS84')
+
+ccsr_biota <- readRDS(file.path(rds.dir, "ccsr_biota.rds")) |> 
+  sf::st_transform('+proj=latlong +datum=WGS84')
 
 
 
 
+
+# SCSR
+scsr_substrate <- readRDS(file.path(rds.dir, "scsr_substrate.rds")) |>
+  sf::st_transform('+proj=latlong +datum=WGS84')
+
+scsr_biota <- readRDS(file.path(rds.dir, "scsr_biota.rds")) |> 
+  sf::st_transform('+proj=latlong +datum=WGS84')
 
 
 
